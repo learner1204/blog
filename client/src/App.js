9ece23ddccb278734d14ed44715dc4c5f68bfa42
Roshan R <72537120+learner1204@users.noreply.github.com>
2023-06-21 00:26:21 +0530

    upld

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" classname="logo">MyBlog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className='post'>
+        <img src="https://neilpatel.com/wp-content/uploads/2023/02/Tips-for-Writing-a-Blog-Post-in-Under-60-Minutes.jpg" alt="guygugu"></img>
+        <h2>Type cd \ into the prompt to go back to the directory.</h2>
+        <p>If you need to navigate from a location back to the main command prompt, this command takes you back immediately.</p>
+      </div>
+    </main>
+
+  );
+}
+
+export default App;
